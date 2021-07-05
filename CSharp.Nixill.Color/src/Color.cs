a67c6c4f5fc6c237033a263d7a8fa442e0307a30
Steven H <github@steven-h.net>
2021-07-05 18:52:21 -0400

    Wait, things don't need to be immutable with structs

--- CSharp.Nixill.Color/src/Color.cs
+++ CSharp.Nixill.Color/src/Color.cs
@@ -2,18 +2,35 @@ using System;
 
 namespace Nixill
 {
-  public class Color
+  public struct Color
   {
-    public float Red { get; internal set; }
-    public float Green { get; internal set; }
-    public float Blue { get; internal set; }
+    public Color(float red, float green, float blue)
+    {
+      Red = red;
+      Green = green;
+      Blue = blue;
+      Alpha = 1;
+    }
+
+    public Color(float red, float green, float blue, float alpha)
+    {
+      Red = red;
+      Green = green;
+      Blue = blue;
+      Alpha = alpha;
+    }
+
+    public float Red;
+    public float Green;
+    public float Blue;
+    public float Alpha;
 
     public float LinearRed
     {
       get => (float)((Red <= 0.04045) ?
         Red / 12.92 :
         Math.Pow((Red + 0.055) / 1.055, 2.4));
-      internal set => Red = (float)((value <= 0.0031308) ?
+      set => Red = (float)((value <= 0.0031308) ?
         value * 12.92 :
         1.055 * Math.Pow(value, 1.0 / 2.4) - 0.055);
     }
@@ -23,7 +40,7 @@ namespace Nixill
       get => (float)((Green <= 0.04045) ?
         Green / 12.92 :
         Math.Pow((Green + 0.055) / 1.055, 2.4));
-      internal set => Green = (float)((value <= 0.0031308) ?
+      set => Green = (float)((value <= 0.0031308) ?
         value * 12.92 :
         1.055 * Math.Pow(value, 1.0 / 2.4) - 0.055);
     }
@@ -33,7 +50,7 @@ namespace Nixill
       get => (float)((Blue <= 0.04045) ?
         Blue / 12.92 :
         Math.Pow((Blue + 0.055) / 1.055, 2.4));
-      internal set => Blue = (float)((value <= 0.0031308) ?
+      set => Blue = (float)((value <= 0.0031308) ?
         value * 12.92 :
         1.055 * Math.Pow(value, 1.0 / 2.4) - 0.055);
     }
