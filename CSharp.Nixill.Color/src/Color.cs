ab8354aa5db020860bfe54fddbb4000b42e41f20
Steven H <github@steven-h.net>
2022-09-17 21:10:52 -0400

    color stuffs

--- CSharp.Nixill.Color/src/Color.cs
+++ CSharp.Nixill.Color/src/Color.cs
@@ -95,14 +95,98 @@ namespace Nixill
         else
         {
           if (Blue > Red) /* ≥ Green */ return Lerp(300, 240, InvLerp(Blue, Green, Red));
-          if /* Red ≥ */ (Green > Blue) return Lerp(60, 0, InvLerp(Red, Blue, Green));
+          if /* Red ≥ */ (Green >= Blue) return Lerp(60, 0, InvLerp(Red, Blue, Green));
           else /* Red ≥ Blue ≥ Green */ return Lerp(300, 360, InvLerp(Red, Green, Blue));
         }
       }
 
       set
       {
-        if (value != value) throw new ArgumentException("NaN hue is not allowed")
+        float val = Value;
+
+        // First let's get the NaN case out of the way.
+        if (float.IsNaN(value))
+        {
+          Red = val;
+          Green = val;
+          Blue = val;
+          return;
+        }
+
+        // And fully saturate the color if there's no saturation.
+        float sat = VSaturation;
+
+        if (sat == 0) VSaturation = 1;
+
+        // Now determine the three values...
+        float hue = value % 360;
+
+        float low = (1 - sat) * val;
+        float high = val;
+
+        float hueIndex = 1 - Math.Abs((value % 120f) - 60f) / 60f;
+
+        float mid = Lerp(low, high, hueIndex);
+
+        // ... and their order.
+        if (hue < 60) { Red = high; Green = mid; Blue = low; }
+        else if (hue < 120) { Green = high; Red = mid; Blue = low; }
+        else if (hue < 180) { Green = high; Blue = mid; Red = low; }
+        else if (hue < 240) { Blue = high; Green = mid; Red = low; }
+        else if (hue < 300) { Blue = high; Red = mid; Green = low; }
+        else { Red = high; Blue = mid; Green = low; }
+      }
+    }
+
+    public float Value
+    {
+      get => Math.Max(Red, Math.Max(Green, Blue));
+      set
+      {
+        float sat = VSaturation;
+
+        if (sat == 0)
+        {
+          Red = value;
+          Green = value;
+          Blue = value;
+        }
+        else
+        {
+          float pVal = Value;
+          Red = Lerp(0, 0, pVal, Red, value);
+          Green = Lerp(0, 0, pVal, Green, value);
+          Blue = Lerp(0, 0, pVal, Blue, value);
+        }
+      }
+    }
+
+    public float VSaturation
+    {
+      get
+      {
+        float low = Math.Min(Red, Math.Min(Green, Blue));
+        float high = Math.Max(Red, Math.Max(Green, Blue));
+
+        return Lerp(high, 0, 0, 1, low);
+      }
+
+      set
+      {
+        float pSat = VSaturation;
+        float val = Value;
+
+        if (pSat == 0)
+        {
+          Red = val;
+          Green = Lerp(0, val, 1, 0, value);
+          Blue = Lerp(0, val, 1, 0, value);
+          return;
+        }
+
+        Red = Lerp(0, val, pSat, Red, value);
+        Green = Lerp(0, val, pSat, Green, value);
+        Blue = Lerp(0, val, pSat, Blue, value);
       }
     }
 
@@ -123,6 +207,14 @@ namespace Nixill
       IntRed = clr.R,
       IntGreen = clr.G,
       IntBlue = clr.B
+    };
+
+    public static Color FromHSV(float hue, float saturation, float value, float alpha = 1)
+    => new Color(1, 0, 0, alpha)
+    {
+      Value = value,
+      VSaturation = saturation,
+      Hue = hue
     };
   }
 }
